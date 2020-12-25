461a08df1d077a1fedb6b1aa6a7e634ca4ded329
MahBoiKrow <76596621+MahBoiKrow@users.noreply.github.com>
2020-12-25 13:44:40 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+var numb1;
+var numb2;
+var result;
+numb1 = prompt("Choose your first number :");
+numb2 = prompt("Choose your second number :");
+result = parseInt(numb1) + parseInt(numb2);
+alert("Result : " + result );
